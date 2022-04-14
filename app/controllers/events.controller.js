66e03a55a5396b256705a245e70e16c189ef279b
Harish Singh <singhharish734@gmail.com>
2022-04-14 17:44:57 +0530

    refactoring event

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,61 +1,65 @@
 const Event = require('../models/event');
 
 module.exports = {
+  showEvents: showEvents,
+  showSingle: showSingle,
+  seedEvents: seedEvents
+}
 
-  // show all Events
-  showEvents: (req, res) => {
 
-    // return view with dummy data
-    res.render('pages/events', {
-      events: events
-    });
-  },
+// show all Events
+function showEvents(req, res) {
 
-  // show a single events
-  showSingle: (req, res) => {
-    // get a single data
-    const event = {
+  // return view with dummy data
+  res.render('pages/events', {
+    events: events
+  });
+}
+
+// show a single events
+function showSingle(req, res) {
+  // get a single data
+  const event = {
+    name: 'BasketBall',
+    slug: 'basketball',
+    description: 'Throwing into Basket'
+  };
+
+  res.render('pages/single', {
+    event: event
+  });
+}
+
+// seed our database
+function seedEvents(req, res) {
+  // create some eventS
+  const events = [{
       name: 'BasketBall',
-      slug: 'basketball',
       description: 'Throwing into Basket'
-    };
+    },
+    {
+      name: 'Swimming',
+      description: 'Phelps is the Fastest Fish'
+    },
+    {
+      name: 'WeightLifting',
+      description: 'Lifting heavy things up'
+    },
+    {
+      name: 'Ping-Pong',
+      description: 'Super fast paddles'
+    }
+  ];
 
-    res.render('pages/single', {
-      event: event
-    });
-  },
+  // use the event model to insert/save
+  // removes all entries and inserts all again from scratch
+  Event.deleteMany({}, () => {
+    for (event of events) {
+      var newEvent = new Event(event);
+      newEvent.save();
+    }
+  });
 
-  // seed our database
-  seedEvents: (req, res) => {
-    // create some eventS
-    const events = [{
-        name: 'BasketBall',
-        description: 'Throwing into Basket'
-      },
-      {
-        name: 'Swimming',
-        description: 'Phelps is the Fastest Fish'
-      },
-      {
-        name: 'WeightLifting',
-        description: 'Lifting heavy things up'
-      },
-      {
-        name: 'Ping-Pong',
-        description: 'Super fast paddles'
-      }
-    ];
-
-    // use the event model to insert/save
-    // removes all entries and inserts all again from scratch
-    Event.deleteMany({}, () => {
-      for (event of events) {
-        var newEvent = new Event(event);
-        newEvent.save();
-      }
-    });
-
-    // seeded msg
-    res.send('Database Seeded');
-  }
-};
+  // seeded msg
+  res.send('Database Seeded');
+}
