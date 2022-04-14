09edcae541b42f0d8fe7ce03a01bbcf4d9ff0369
Harish Singh <singhharish734@gmail.com>
2022-04-14 16:11:36 +0530

    dotenv + mongo crash handled

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-// grab dependencies
+// load environment variables
+require('dotenv').config();
+
+ // grab dependencies
 const express= require('express'),
 app= express(),
 port= process.env.PORT || 8080,
-expressLayouts= require('express-ejs-layouts');
+expressLayouts= require('express-ejs-layouts'),
+mongoose= require('mongoose')
+;
 
-// configure application
+// configure application ================================
 
 // telling express where to look for static assets
 app.use(express.static(__dirname + '/public'));
 
 // set ejs as templating engine
 app.set('view engine', 'ejs');
-app.use(expressLayouts); 
+app.use(expressLayouts);
 
-// set the route
+// connect to database
+mongoose.connect(process.env.DB_URI);
+
+// set the route ========================================
 app.use(require('./app/routes'));
 
-// start our server
+// start our server =====================================
 app.listen(port, () =>{
   console.log(`APP LISTENING ON http://localhost:${port}`);
 });
